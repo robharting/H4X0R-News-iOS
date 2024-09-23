45212fa5e80cdebda34f5912bc20eb1e1f778dc4
Rob Harting <robharting@hotmail.com>
2024-09-23 17:09:16 +0200

    first setup of H4X0R-News-iOS

--- H4X0R-News-iOS/ContentView.swift
+++ H4X0R-News-iOS/ContentView.swift
@@ -9,16 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationBarTitle("H4X0R NEWS")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola")
+]
+
