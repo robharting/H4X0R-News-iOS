bfb27cd80a6fb76c40a0bead64036fe2ab3556e4
Rob Harting <robharting@hotmail.com>
2024-09-23 17:05:38 +0200

    Initial Commit

---
+++ H4X0R-News-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  H4X0R-News-iOS
+//
+//  Created by Harting, R.P.G. (Rob) on 23/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ H4X0R-News-iOS/H4X0R_News_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_News_iOSApp.swift
+//  H4X0R-News-iOS
+//
+//  Created by Harting, R.P.G. (Rob) on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_News_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
